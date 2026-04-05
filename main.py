141803244a3ed01bcf144773f8891bbba7c9bc18
AnnaKiira <annakirath@gmail.com>
2026-04-05 22:31:53 +0200

    Connected my api news key

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+import os
+import httpx
+
+load_dotenv()
+api_key = os.environ.get('NEWS_API_KEY')
 
 app = FastAPI()
 
@@ -18,3 +24,8 @@ app.add_middleware(
 @app.get("/health")
 def read_root():
     return {"status": "OK"}
+
+@app.get("/newsarticles")
+def get_news_articles():
+    response = httpx.get(f"https://newsapi.org/v2/top-headlines?country=us&apiKey={api_key}")
+    return response.json()
